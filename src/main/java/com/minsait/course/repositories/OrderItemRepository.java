d0606088097d99fce03ad6495de1ff148ad880be
Matheus Gava <mgava@minsait.com>
2021-10-22 15:59:51 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/minsait/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.minsait.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.minsait.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
